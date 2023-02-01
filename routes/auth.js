fd42d1353817ca4d0aad558ec3d0795771348919
patriciacostadacruz <patriciacstcz@gmail.com>
2023-02-01 15:02:57 +0100

    Fixes some tipos and improves login path and redirection

--- routes/auth.js
+++ routes/auth.js
@@ -4,61 +4,40 @@ const User = require('../models/User.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
 // @desc    Displays form view to sign up
 // @route   GET /auth/signup
 // @access  Public
-
-//get signup view
 router.get('/signup', async (req, res, next) => {
   res.render('auth/signup');
-  console.log("signup!")
 })
-
-//post signup
-router.post('/signup', async (req, res, next) => {
-  const { username, firstName , lastName, email, password, cookingLevel } = req.body;
-  if (!username || !firstName || !lastName || !email || !password || !cookingLevel) {
-    res.render('auth/signup', { error: 'Please fill all data to signup' });
-    return;
-  } 
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!regex.test(password)) {
-    res.render("auth/signup", { error: "Password needs to contain at least 6 characters, one number and one lowercase and uppercase character."});
-    return;
-  }
-//Check if user exists ... its correct?
-  const userInDB = await User.findOne({ email: email });
-  if (userInDB) {
-    res.render('auth/signup' , { error: `${email} already exists!`});
-    return;
-  }
-  
-  try {
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-    const user = await User.create({ username, firstName , lastName, email, hashedPassword, cookingLevel });
-    res.redirect('/');
-//redirect to Profile once the signup its done
-  } catch (error) { 
-    next (error);
-  }
-});
 
 
 // @desc    Sends user auth data to database to create a new user
 // @route   POST /auth/signup
 // @access  Public
 router.post('/signup', async (req, res, next) => {
-  const { email, password, username } = req.body;
-  // ⚠️ Add validations!
+  const { username, firstName , lastName, email, password, cookingLevel } = req.body;
+  if (!username || !firstName || !lastName || !email || !password || !cookingLevel) {
+    res.render('auth/signup', { error: 'Please fill all data to sign up.' });
+    return;
+  } 
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.render("auth/signup", { error: "Password needs to contain at least 6 characters, one number, one special character and one lowercase and uppercase character."});
+    return;
+  }
+  const userInDB = await User.findOne({ email: email });
+  if (userInDB) {
+    res.render('auth/signup', { error: `${email} already exists!`});
+    return;
+  }
   try {
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const user = await User.create({ username, email, hashedPassword });
-    res.render('auth/profile', user)
-  } catch (error) {
-    next(error)
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = await User.create({ username, firstName , lastName, email, hashedPassword, cookingLevel });
+    res.render('auth/login');
+  } catch (error) { 
+    next (error);
   }
 });
 
@@ -87,7 +66,7 @@ router.post('/login', async (req, res, next) => {
       const match = await bcrypt.compare(password, user.hashedPassword);
       if (match) {
         req.session.currentUser = user;
-        res.redirect('/', user);
+        res.render('auth/profile', user);
       } else {
         res.render('auth/login', { error: "Unable to authenticate user." });
       }
@@ -99,7 +78,7 @@ router.post('/login', async (req, res, next) => {
 
 // @desc    Destroy user session and log out
 // @route   POST /auth/logout
-// @access  Private 
+// @access  User 
 router.post('/logout', (req, res, next) => {
   req.session.destroy((err) => {
     if (err) {

7285b8bdf5a3cd5ab0e9b3611082785319fafbda
patriciacostadacruz <patriciacstcz@gmail.com>
2023-02-06 13:07:24 +0100

    Allows user to reset password + improves profile views

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.js');
+const Recipe = require("../models/Recipe");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const isLoggedIn = require('../middlewares/index');
@@ -35,7 +36,6 @@ router.post('/signup', async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashedPassword = bcrypt.hashSync(password, salt);
-    console.log(username, firstName , lastName, email, password, cookingLevel);
     const user = await User.create({username, firstName, lastName, email, hashedPassword, cookingLevel});
     res.render('auth/login');
   } catch (error) { 
@@ -68,12 +68,61 @@ router.post('/login', async (req, res, next) => {
       const match = await bcrypt.compare(password, user.hashedPassword);
       if (match) {
         req.session.currentUser = user;
-        res.render('index', {user});
+        const recipes = await Recipe.find();
+        res.render('index', {user, recipe: recipes});
       } else {
         res.render('auth/login', {error: "Unable to authenticate user."});
       }
     }
   } catch (error) {
+    next(error);
+  }
+});
+
+// @desc    Shows reset password page
+// @route   GET /auth/password-reset
+// @access  User
+router.get("/password-reset", isLoggedIn, async (req, res, next) => {
+  const user = req.session.currentUser;
+  try {
+    const userDB = await User.findOne({_id: user._id});
+    res.render("auth/passwordReset", {user: userDB});
+  } catch(error) {
+    next(error);
+  }
+});
+
+// @desc    Resets password 
+// @route   POST /auth/password-reset
+// @access  User
+router.post("/password-reset", isLoggedIn, async (req, res, next) => {
+  const user = req.session.currentUser;
+  const {oldPassword, newPassword, confirmPassword} = req.body;
+  if (!oldPassword || !newPassword || !confirmPassword) {
+    res.render("auth/passwordReset", {error: "You need to fill the 3 fields in order to reset your password"});
+    return;
+  }
+  const match = await bcrypt.compare(oldPassword, user.hashedPassword);
+  if (!match) {
+    res.render("auth/passwordReset", {error: "Old password isn't correct."});
+    return;
+  };
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(newPassword)) {
+    res.render("auth/passwordReset", {error: "Password needs to contain at least 6 characters, one number, one special character and one lowercase and uppercase character."});
+    return;
+  }
+  if (newPassword !== confirmPassword) {
+    res.render("auth/passwordReset", {error: "You need to confirm your password by re-writing the same password as the new desired one."});
+    return;
+  }
+  try {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(newPassword, salt);
+    const userDB = await User.findByIdAndUpdate({_id: user._id}, {hashedPassword});
+    req.session.currentUser = userDB;
+    res.render('auth/login');
+  } catch(error) {
     next(error);
   }
 });
